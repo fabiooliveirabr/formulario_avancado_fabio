a341a765be977826b7a3f551e31848fae0f26ca0
Fabio Oliveira <fabiodeoliveirabr@gmail.com>
2023-08-25 20:18:06 -0300

    Projeto aprimorado

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,8 +12,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   DateTime dataSelecionada = DateTime.now();
   String _opcaoPadrao = "Selecione";
-  List<String> _listaOpcoes = 
-  ["Selecione", "Opção 1", "Opção 2", "Opção 3"];
+  List<String> _listaOpcoes = ["Selecione", "Opção 1", "Opção 2", "Opção 3"];
   bool _opRecife = false;
   bool _opJaboatao = false;
   bool _opCabo = false;
@@ -23,202 +22,223 @@ class _HomePageState extends State<HomePage> {
   String _opcaoEscolhida = "";
   List<String> _outraLista = ["A+", "A-", "O-", "AB+"];
 
-    Future<void> _escolherData(BuildContext context) async {
-      final picked = await showDatePicker(
+  Future<void> _escolherData(BuildContext context) async {
+    final picked = await showDatePicker(
       context: context,
       initialDate: dataSelecionada,
       firstDate: DateTime(2000),
       lastDate: DateTime(2101),
-      );
-      if (picked != null && picked != dataSelecionada)
-        setState(() {
+    );
+    if (picked != null && picked != dataSelecionada)
+      setState(() {
         dataSelecionada = picked;
-        });
-      }
-
+      });
+  }
 
   //Esse comando criar o ID do formulário
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  
+
   void _submitForm() {
-    if(_opcaoEscolhida==""){
-        setState(() {
-           corDaMensagem2 = Colors.red;
-        });       
-    }else{
-        setState(() {
-           corDaMensagem2 = Colors.transparent;
-        });  
+    if (_opcaoEscolhida == "") {
+      setState(() {
+        corDaMensagem2 = Colors.red;
+      });
+    } else {
+      setState(() {
+        corDaMensagem2 = Colors.transparent;
+      });
     }
 
-    if(_opRecife == false && _opCabo == false && _opJaboatao == false){
-        setState(() {
-           corDaMensagem = Colors.red;
-        });
-    }else{
-        setState(() {
-           corDaMensagem = Colors.transparent;
-        });
-    //Exibir o conteúdo das variáveis
-    print("$dataSelecionada");
-    print("Recife: $_opRecife");
-    print("Cabo de Santo Agostinho: $_opCabo");
-    print("Jaboatão: $_opJaboatao");
-    print("$_opcaoPadrao");
-    print("$_opcaoEscolhida");
-
+    if (_opRecife == false && _opCabo == false && _opJaboatao == false) {
+      setState(() {
+        corDaMensagem = Colors.red;
+      });
+    } else {
+      setState(() {
+        corDaMensagem = Colors.transparent;
+      });
+      //Exibir o conteúdo das variáveis
+      print("$dataSelecionada");
+      print("Recife: $_opRecife");
+      print("Cabo de Santo Agostinho: $_opCabo");
+      print("Jaboatão: $_opJaboatao");
+      print("$_opcaoPadrao");
+      print("$_opcaoEscolhida");
     }
 
-  if (_formKey.currentState!.validate()) {
-  // Fazer alguma ação depois de validar
-  print('Está tudo certo');
-      }
+    if (_formKey.currentState!.validate()) {
+      // Fazer alguma ação depois de validar
+      print('Está tudo certo');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
             children: [
-            Text("FORMULÁRIO AVANÇADO"),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  //-----------------------------
-                  TextFormField(
-                    validator: (value) => _validateEmail(value.toString()),
-                    decoration: InputDecoration(
-                      label: Text("E-mail"),
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.email),
-                    ),
-                  ),
-                  //-----------------------------
-                  DropdownButtonFormField<String>(
-                    validator: (value) => _validarComboBox(value.toString()),
-                    value: _opcaoPadrao,
-                    onChanged: (novoValor) {
-                    setState(() {
-                    _opcaoPadrao = novoValor.toString();
-                    });
-                    },
-                    items: _listaOpcoes.map((opcao) {
-                    return DropdownMenuItem<String>(
-                    value: opcao,
-                    child: Text(opcao),
-                    );
-                    }).toList(),
-                    decoration: InputDecoration(
-                    labelText: 'Opções',
-                    ),
-                    ),
-                //-----------------------------
-
-                CheckboxListTile(
-                  title: Text('Recife'),
-                  value: _opRecife,
-                  onChanged: (bool? value) {
-                  setState(() {
-                  _opRecife = value!;
-                  });
-                  },
-                  ),
-                  CheckboxListTile(
-                  title: Text('Jaboatão'),
-                  value: _opJaboatao,
-                  onChanged: (bool? value) {
-                  setState(() {
-                  _opJaboatao = value!;
-                  });
-                  },
-                  ),
-                  CheckboxListTile(
-                  title: Text('Cabo de Santo Agostinho'),
-                  value: _opCabo,
-                  onChanged: (bool? value) {
-                  setState(() {
-                  _opCabo = value!;
-                  });
-                  },
-                  ),
-
-                
-                //-----------------------------
-                Text("Selecione pelo menos uma cidade",
-                  style: TextStyle(color: corDaMensagem),
-                ),
-                //-----------------------------
-
-                Column(
-                  children: _outraLista.map((cursos) {
-                  return RadioListTile(
-                  title: Text(cursos),
-                  value: cursos,
-                  groupValue: _opcaoEscolhida,
-                  onChanged: (value) {
-                  setState(() {
-                  _opcaoEscolhida = value.toString();
-                  });
-                  },
-                  );
-                  }).toList(),
-                  ),
-
-                Text("Selecione um tipo sanguíneo",
-                  style: TextStyle(color: corDaMensagem2),
-                ),
-                
-                //-----------------------------
-                Row(
+              Text(
+                "FORMULÁRIO AVANÇADO",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
                   children: [
-                    Text("${dataSelecionada.toLocal()}".split(' ')[0]),
-                    ElevatedButton(
-                      onPressed: () => _escolherData(context), 
-                      child: Text("Escolha uma data"),),
+                    //-----------------------------
+                    TextFormField(
+                      validator: (value) => _validateEmail(value.toString()),
+                      decoration: InputDecoration(
+                        label: Text("E-mail"),
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                    ),
+                    //-----------------------------
+                    SizedBox(height: 10),
+                    //-----------------------------
+                    DropdownButtonFormField<String>(
+                      validator: (value) => _validarComboBox(value.toString()),
+                      value: _opcaoPadrao,
+                      onChanged: (novoValor) {
+                        setState(() {
+                          _opcaoPadrao = novoValor.toString();
+                        });
+                      },
+                      items: _listaOpcoes.map((opcao) {
+                        return DropdownMenuItem<String>(
+                          value: opcao,
+                          child: Text(opcao),
+                        );
+                      }).toList(),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Opções',
+                      ),
+                    ),
+                    //-----------------------------
+                    SizedBox(height: 20),
+                    Text(
+                      "Escolha uma(s) cidade(s)",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    //-----------------------------
+                    CheckboxListTile(
+                      title: Text('Recife'),
+                      value: _opRecife,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _opRecife = value!;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      title: Text('Jaboatão'),
+                      value: _opJaboatao,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _opJaboatao = value!;
+                        });
+                      },
+                    ),
+                    CheckboxListTile(
+                      title: Text('Cabo de Santo Agostinho'),
+                      value: _opCabo,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _opCabo = value!;
+                        });
+                      },
+                    ),
 
+                    //-----------------------------
+                   Text(
+                      "Selecione pelo menos uma cidade",
+                      style: TextStyle(color: corDaMensagem),
+                    ),
+                    //-----------------------------
+                     Text(
+                      "Escolha um tipo sanguíneo",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    //-----------------------------
+
+                    Column(
+                      children: _outraLista.map((cursos) {
+                        return RadioListTile(
+                          title: Text(cursos),
+                          value: cursos,
+                          groupValue: _opcaoEscolhida,
+                          onChanged: (value) {
+                            setState(() {
+                              _opcaoEscolhida = value.toString();
+                            });
+                          },
+                        );
+                      }).toList(),
+                    ),
+
+                    Text(
+                      "Por favor, selecione um tipo sanguíneo",
+                      style: TextStyle(color: corDaMensagem2),
+                    ),
+
+                    //-----------------------------
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("${dataSelecionada.toLocal()}".split(' ')[0]),
+                        ElevatedButton(
+                          onPressed: () => _escolherData(context),
+                          child: Text("Escolha uma data"),
+                        ),
+                      ],
+                    ),
+                    //-----------------------------
+                    SizedBox(height: 10),
+                    //-----------------------------
+                    ElevatedButton(
+                      onPressed: () {
+                        _submitForm();
+                      },
+                      child: Text("Enviar"),
+                    ),
+                    //-----------------------------
                   ],
                 ),
-
-                //-----------------------------
-                  ElevatedButton(
-                    onPressed: (){ _submitForm();}, 
-                    child: Text("Enviar"),),
-                  //-----------------------------
-                ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-
-
 String? _validateEmail(String texto) {
 //Verificar se está vazio
-if (texto.isEmpty) {
-return 'Por favor, insira um email.';
-}
+  if (texto.isEmpty) {
+    return 'Por favor, insira um email.';
+  }
 //Verificar se não o @
-if (!texto.contains('@') || !texto.contains(".")) {
-return 'Por favor, insira um email válido.';
-}
+  if (!texto.contains('@') || !texto.contains(".")) {
+    return 'Por favor, insira um email válido.';
+  }
 //Verificar se tem espaços
-if (texto.contains(' ')){
-return 'Por favor, não coloque espaços em branco.';
-}
+  if (texto.contains(' ')) {
+    return 'Por favor, não coloque espaços em branco.';
+  }
 //Se estiver tudo certo retorna null
-return null;
+  return null;
 }
 
 //Criar método para validar o ComboBox
-String? _validarComboBox(String texto){
-    if(texto == "Selecione"){
-        return "Por favor, selecione uma opção";
-    }
-    return null;
+String? _validarComboBox(String texto) {
+  if (texto == "Selecione") {
+    return "Por favor, selecione uma opção";
+  }
+  return null;
 }
